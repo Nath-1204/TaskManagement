d6a578e8aac956170f77cf54892e887697dc16d1
Nath-1204 <mamynatacha19@gamil.com>
2026-02-19 17:41:14 +0300

    changes

--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -4,7 +4,7 @@ import { useSearchParams } from 'react-router-dom'
 import { assets } from '../assets/assets';
 import toast from "react-hot-toast";
 import { format } from "date-fns";
-import { MessageCircle } from "lucide-react";
+import { CalendarIcon, MessageCircle, PenIcon } from "lucide-react";
 
 
 const TaskDetails = () => {
@@ -114,7 +114,86 @@ const TaskDetails = () => {
           </div>
 
           {/* Add Comment */}
+          <div className="flex flex-col sm:flex-row items-start sm:items-end gap-3">
+            <textarea 
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              placeholder='Ecrire un commentaire...'
+              className='w-full dark:bg-zinc-800 border border-gray-300 dark:border-zinc-700 rounded-md p-2 
+              text-sm text-gray-900 dark:text-zinc-200  resize-none focus:outline-none focus:ring-1 focus:ring-blue-600'
+              rows={3}
+            />
+            <button
+              onClick={handleAddComment}
+              className='bg-gradient-to-l from-blue-500 to-blue-600 transition-colors text-white text-sm px-5 py-2 rounded'
+            >
+              Post
+            </button>
+    
+          </div>
         </div>
+      </div>
+
+      {/* Right: Task + Project Info */}
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        {/* Task Info */}
+        <div className="p-5 rounded-md bg-white dark:bg-zinc-900 border border-gray-300 dark:border-zinc-800">
+          <div className='mb-3'>
+            <h1 className='text-lg font-medium text-gray-900 dark:text-zinc-100'>{task.title}</h1>
+            <div className="flex flex-wrap gap-2 mt-2">
+              <span className='px-2 py-0.5 rounded bg-zinc-200 dark:bg-zinc-700 text-zinc-900 dark:text-blue-300 text-xs'>
+                {task.status}
+              </span>
+              <span className='px-2 py-0.5 rounded bg-zinc-200 dark:bg-zinc-700 text-zinc-900 dark:text-blue-300 text-xs'>
+                {task.type}
+              </span>
+              <span className='px-2 py-0.5 rounded bg-green-200 dark:bg-emerald-900 text-green-900 dark:text-emerald-300 text-xs'>
+                {task.priority}
+              </span>
+            </div>
+          </div>
+
+          {
+            task.description && (
+              <p className='text-sm text-gray-600 dark:text-zinc-400 leading-relaxed mb-4'>{task.description}</p>
+            )
+          }
+
+          <hr className="border-zinc-200 dark:border-zinc-700 my-3" />
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 text-sm text-gray-700 dark:text-zinc-300">
+            <div className="flex itelms-center gap-2">
+              <img src={task.assignee?.image} className='size-5 rounded-full' alt="avatar" />
+              {task.assignee?.name || "Unassigned"}
+            </div>
+            <div className="flex items-center gap-2">
+              <CalendarIcon className='size-4 text-gray-500 dark:text-zinc-500' />
+              Due : {format(new Date(task.due_date), "dd MM yyyy")}
+            </div>
+          </div>
+
+        </div>
+
+        {/* Project Info */}
+        {
+          project && (
+            <div>
+              <p className="text-xl font-medium mb-4">Project Details</p>
+              <h2 className="text-gray-900 dark:text-zinc-100 flex items-center gap-2">
+                <PenIcon className='size-4'/> {project.name}
+              </h2>
+              <p className="text-xs mt-3">
+                Project Start Date:
+                {format(new Date(project.start_date), "dd MMM yyy")}
+              </p>
+              <div className="flex flex-wrap gap-4 text-sm text-gray-500 dark:text-zinc-400 mb-3">
+                <span>Status: {project.status}</span>
+                <span>Priority: {project.priority}</span>
+                <span>Progress: {project.progress}%</span>
+              </div>
+            </div>
+          )
+        }
       </div>
       
     </div>
